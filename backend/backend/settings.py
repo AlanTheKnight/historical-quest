c3201ed1791a98bae7438aa6b1fcfcabc12cc2ec
Maksim Gusev <51376126+AlanTheKnight@users.noreply.github.com>
2023-12-07 20:12:01 +0300

    Add DRF Token auth, fix bug

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     "imagekit",
     "backend.api",
     "django_cleanup",
+    "rest_framework.authtoken",
 ]
 
 MIDDLEWARE = [
@@ -138,3 +139,14 @@ REST_FRAMEWORK = {"DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.Dja
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = config["Django"]["MEDIA_ROOT"]
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    # "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
+}
